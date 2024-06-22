ecfd1faf59cd3f4655d90f43d6c23efc004c57d3
Zain Arshad <56497550+zainarshad62452@users.noreply.github.com>
2024-06-22 15:43:05 +0500

    Update configMaps.dart

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -2,14 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'Models/allUsers.dart';
 import 'dart:convert';
-// String mapKey = "AIzaSyCev2LI2DLPWPAqQkbPs0hSrJdL989c2Bc";
-// String mapKey = "AIzaSyDWt1GqrbFbH1fnH40LRytjU9l4ekHHycE";
-String mapKey = "AIzaSyC_n2DzkMvHK_wxXAGPm3Klk75LtyIwJ0I";
+
+String mapKey = "Your Map Key";
 
 const String stripePublishableKey = 'YOUR_PUBLISHABLE_KEY';
 const String stripeSecretKey = 'YOUR_SECRET_KEY';
 
-String serverToken = "key=AAAAGrkP1-w:APA91bG8uQKibkTjEETsleHKlNgxhYi_t-ayXmoDkSgEQdkNiYwqxJcNyOxG9lhwEI9kt5IvqcbkR4fTiETj2dWlfYa-8xaA_5qCvwsVo42puabWchkL6sztf1I2m2szlf_9lyThhxTe";
+String serverToken = "Your Server Token";
 
 User? firebaseUser;
 
